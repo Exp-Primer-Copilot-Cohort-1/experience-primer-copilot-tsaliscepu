a59356de2d31a5b5a371a9dcbbe41e396ca78eb8
Tsalis Khoiruddin <tsalis20230100155@sibermu.ac.id>
2024-01-22 05:46:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,77 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML',
+            image: 'https://cdn.svgporn.com/logos/html-5.svg'
+        },
+        {
+            name: 'CSS',
+            image: 'https://cdn.svgporn.com/logos/css-3.svg'
+        },
+        {
+            name: 'Javascript',
+            image: 'https://cdn.svgporn.com/logos/javascript.svg'
+        },
+        {
+            name: 'PHP',
+            image: 'https://cdn.svgporn.com/logos/php.svg'
+        },
+        {
+            name: 'Laravel',
+            image: 'https://cdn.svgporn.com/logos/laravel.svg'
+        },
+        {
+            name: 'VueJS',
+            image: 'https://cdn.svgporn.com/logos/vue.svg'
+        },
+        {
+            name: 'ReactJS',
+            image: 'https://cdn.svgporn.com/logos/react.svg'
+        },
+        {
+            name: 'Python',
+            image: 'https://cdn.svgporn.com/logos/python.svg'
+        },
+        {
+            name: 'Django',
+            image: 'https://cdn.svgporn.com/logos/django.svg'
+        },
+        {
+            name: 'MySQL',
+            image: 'https://cdn.svgporn.com/logos/mysql.svg'
+        },
+        {
+            name: 'MongoDB',
+            image: 'https://cdn.svgporn.com/logos/mongodb.svg'
+        },
+        {
+            name: 'Git',
+            image: 'https://cdn.svgporn.com/logos/git-icon.svg'
+        },
+        {
+            name: 'Docker',
+            image: 'https://cdn.svgporn.com/logos/docker-icon.svg'
+        },
+        {
+            name: 'Jenkins',
+            image: 'https://cdn.svgporn.com/logos/jenkins.svg'
+        },
+        {
+            name: 'AWS',
+            image: 'https://cdn.svgporn.com/logos/aws.svg'
+        },
+        {
+            name: 'Digital Ocean',
+            image: 'https://cdn.svgporn.com/logos/digital-ocean.svg'
+        },
+        {
+            name: 'Nginx',
+            image: 'https://cdn.svgporn.com/logos/nginx.svg'
+        },
+        {
+            name: 'Apache',
+            image: 'https://cdn.svgporn.com/logos/apache.svg'
+        },
+        {
+            name: 'Linux',
+            image: 'https://cdn.svgporn.com/logos
